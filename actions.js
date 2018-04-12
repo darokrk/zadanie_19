3b016bfc59ce4b513dda11ee65ffcf339e7be244
Dariusz Rosiek <daro.krk@interia.pl>
2018-04-12 19:25:33 +0200

    Create reducer.js and comments.js

--- actions.js
+++ actions.js
@@ -1,16 +1,16 @@
 import uuid from uuid;
 
-const ADD_COMMENT = 'ADD_COMMENT';
-const REMOVE_COMMENT = 'REMOVE_COMMENT';
-const EDIT_COMMENT = 'EDIT_COMMENT';
-const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
-const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
 
-const boundAddComment = text => dispatch(addComment(text));
-const boundEditComment = (text, id) => dispatch(editComment(text, id));
-const boundRemoveComment = id => dispatch(removeComment(id));
-const boundThumbUpComment = id => dispatch(thumbUpComment(id));
-const boundThumbDownComment = id => dispatch(thumbDownComment(id));
+export const boundAddComment = text => dispatch(addComment(text));
+export const boundEditComment = (text, id) => dispatch(editComment(text, id));
+export const boundRemoveComment = id => dispatch(removeComment(id));
+export const boundThumbUpComment = id => dispatch(thumbUpComment(id));
+export const boundThumbDownComment = id => dispatch(thumbDownComment(id));
 
 function addComment(text) {
     return {
